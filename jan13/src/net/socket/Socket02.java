4da0b6ba2f473b29a179c9d09abf856c7dfeb749
wjdahr125@gmail.com <60291344+Yuj-Jung-Mok@users.noreply.github.com>
2023-01-16 09:06:39 +0900

    이름 변경

--- jan13/src/net/socket/Socket02.java
+++ jan13/src/net/socket/Socket02.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 
-public class Sockey02 {
+public class Socket02 {
 
 	public static void main(String[] args) {
 		String serverIP = "127.0.0.1"; // localhost
